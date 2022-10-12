99c914a88df22c06dbcae826780435ea3c147e18
60manojkumar@gmail.com <Motivated@2019>
2022-10-12 20:06:19 +0530

    DEMO COMMIT 2

--- src/variables/Test.java
+++ src/variables/Test.java
@@ -5,6 +5,10 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("THis is demo for git ");
+        System.out.println("Test");
 
+
+
+        System.out.println("Test");
     }
 }
