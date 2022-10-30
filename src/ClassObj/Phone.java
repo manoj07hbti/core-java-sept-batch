740905e1185d65861ac0048c1687fb7e18795a27
A-s-shukla-9454 <shuklajianurag995@gmail.com>
2022-10-30 08:50:49 +0530

    test comit

---
+++ src/ClassObj/Phone.java
@@ -0,0 +1,30 @@
+package ClassObj;
+
+public class Phone {
+
+
+
+        String brand="Mi";
+        String model="vi20";
+        String GB="500";
+        int ram=16;
+        String colour="Red";
+
+        public static void main(String[]args){
+
+                Phone obj=new Phone();
+                System.out.println("printing the phone model:"+obj.brand);
+                System.out.println("Printing the phone model:"+obj.model);
+                System.out.println("Printing the phone GB:"+obj.colour);
+                System.out.println("printing the phone ram:"+obj.ram);
+
+
+
+
+        }
+
+
+    }
+
+
+
