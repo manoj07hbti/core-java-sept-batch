0e181ae595ffa955410cc8d8c0eb533b6a7ad44d
1994Khushi <ayangelyoung@gmai.com>
2022-11-01 00:00:24 +0530

    building

---
+++ src/khushi_gautam/building.java
@@ -0,0 +1,18 @@
+package khushi_gautam;
+
+public class building {
+    String name = "Rupali homes";
+    int Floor = 20;
+    int Room = 200;
+    int Gate = 4;
+
+    public static void main(String args[]){
+        //todo syntax classname objname = new classname();
+        building obj = new building();
+        // todo objname.ropertyname
+        System.out.println("Name : "+obj.name);
+        System.out.println("Floor: "+obj.Floor);
+        System.out.println("Room : "+obj.Room);
+        System.out.println("Gate : "+obj.Gate);
+    }
+}
