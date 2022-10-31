7aad016a596f80a28e08a85a837c52352fa3a312
1994Khushi <ayangelyoung@gmai.com>
2022-10-31 19:57:38 +0530

    khushi_Array city

---
+++ src/khushi_gautam/City.java
@@ -0,0 +1,17 @@
+package khushi_gautam;
+
+public class City {
+    String city1 ="Aligarh";
+    String city2 ="Agra";
+    String city3 ="Bulandshahr";
+    String city4 ="Gaziabad";
+    String city5 ="Delhi";
+
+    public static void main(String args[]){
+        //todo classname objname = new clasname();
+        City obj = new City();
+        // todo objname.propertyname
+        System.out.println(obj);
+        System.out.println(obj.city1+","+obj.city2+","+obj.city3+","+obj.city4+","+obj.city5);
+    }
+}
