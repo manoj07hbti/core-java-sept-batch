6552ea037de01d2d35913e38d5fffec05376a6df
Ajeet Chaudhary <ajeetc775@gmail.com>
2022-10-31 14:48:53 +0530

    assign 1

---
+++ src/arithmetic_problems/Cube.java
@@ -0,0 +1,8 @@
+package arithmetic_problems;
+
+public class Cube {
+    public static void main(String[] args) {
+        int number=4;
+        System.out.println("arithmetic_problems.Cube of "+number+" is "+number*number*number);
+    }
+}
