3ca3e5f5ebf20d9feeeb2e887de1757607a2c2a6
Gyanu15R <gyanendrakumar.infoicon@gmail.com>
2022-10-31 17:36:17 +0530

    test commit

--- src/my_class_and_object/City.java
+++ src/my_class_and_object/City.java
@@ -1,4 +1,24 @@
 package my_class_and_object;
+import java.util.Scanner;
 
 public class City {
+    String name;
+    int population;
+
+    public void cityDetail() {
+
+
+        System.out.println("Enter Your City Name :");
+        name = new Scanner(System.in).nextLine();
+        System.out.println("Enter Your City Population :");
+        population = new Scanner(System.in).nextInt();
+
+    }
+
+    public static void main(String args[]) {
+        City obj = new City();
+        obj.cityDetail();
+        System.out.println(" "+obj.name);
+        System.out.println(" "+obj.population);
+    }
 }
