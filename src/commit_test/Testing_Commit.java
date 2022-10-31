2bc9367c5e0e991044fd657b03acac055ac6e2c1
amangautam11698 <amangautam11698@gmail.com>
2022-10-31 17:05:38 +0530

    Testing Commit

---
+++ src/commit_test/Testing_Commit.java
@@ -0,0 +1,9 @@
+package commit_test;
+
+public class Testing_Commit {
+
+    public static void main(String[] args){
+
+        System.out.println("This is a Testing Program For GIT Commit&Push");
+    }
+}
