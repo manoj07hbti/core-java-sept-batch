d03e0a9ef0933d327f637a7e02db661410afad1b
1994Khushi <ayangelyoung@gmai.com>
2022-11-01 00:09:39 +0530

    table.java

---
+++ src/khushi_gautam/table.java
@@ -0,0 +1,23 @@
+package khushi_gautam;
+
+public class table {
+    String name = "Table";
+    String company = "Harkut";
+    int countity = 18;
+    String address =" B-15, Sai Bihar Aligarh";
+
+    public static void main(String[] args) {
+//TODO SYNTAX :  ClassName objName= new ClassName();
+
+        table obj= new table(); // obj object created
+
+        System.out.println("Printing object "+obj);
+//TODO objName.propertyname
+
+        System.out.println(" name :"+obj.name);
+        System.out.println("company :"+obj.company);
+        System.out.println("countity :"+obj.countity);
+        System.out.println("address :"+ obj.address);
+
+    }
+}
