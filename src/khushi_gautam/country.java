b50cd5901deca2272475974efed0a45d5f6a5421
1994Khushi <ayangelyoung@gmai.com>
2022-11-01 00:04:58 +0530

    building

---
+++ src/khushi_gautam/country.java
@@ -0,0 +1,17 @@
+package khushi_gautam;
+
+public class country {
+    String a = "India";
+    String b = "Australia";
+    String c = "America";
+    String d = "Ukraine";
+    String e = "Russia";
+    String f = "Hong Kong";
+
+    public static void main(String[] args) {
+        //todo classname objname = new classname();
+        country obj = new country();
+        //todo objname.propertyname
+        System.out.println(obj.a + "," + obj.b + "," + obj.c + ","+obj.d+","+obj.e+","+obj.f);
+    }
+}
