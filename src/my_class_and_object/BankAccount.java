95b9c7015f98dfdd1a99a9b36e83507f687fc882
Gyanu15R <gyanendrakumar.infoicon@gmail.com>
2022-11-01 18:18:22 +0530

    Assign2_BankAccount

--- src/my_class_and_object/BankAccount.java
+++ src/my_class_and_object/BankAccount.java
@@ -16,6 +16,8 @@ public class BankAccount {
     public static void main(String[] args) {
         BankAccount obj = new BankAccount(01034121,"Gyanendra", "Jhansi");
         System.out.println("Account NO. :"+obj.acctNo + "   Account Name :"+obj.acctName +"    Branch Name :" +obj.branchName);
+        BankAccount obj1 = new BankAccount(01034451,"Somendra", "Mauranipur");
+        System.out.println("Account NO. :"+obj1.acctNo + "   Account Name :"+obj1.acctName +"    Branch Name :" +obj1.branchName);
     }
 
 }
