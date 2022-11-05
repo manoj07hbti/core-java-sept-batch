eccae9374251feeccab41243e2e29bf481642922
VISHAL BABOO <vishalbabu503@gmail.com>
2022-11-05 11:52:34 +0530

    cube commit

--- src/methods/Cube.java
+++ src/methods/Cube.java
@@ -1,16 +1,21 @@
 package methods;
 
 public class Cube {
-    public int cube(){
-        int a=3;
-        int cube=a*a*a;
-        System.out.println("cube of "+a+":");
-        return  cube;
+    public String even_and_odd() {
+        long number = 244447844125512l;
+        if (number % 2 == 0)
+            return "number is even:" + number;
+        else{
+            return"number is odd:"+number;
+        }
+
     }
 
     public static void main(String[] args) {
         Cube obj=new Cube();
-        int result=obj.cube();
+        String result=obj.even_and_odd();
         System.out.println(result);
     }
 }
+
+
