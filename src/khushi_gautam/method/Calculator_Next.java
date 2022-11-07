130cea5a91b67d68df656d857506c8bd15ae4a9e
1994Khushi <ayangelyoung@gmai.com>
2022-11-07 18:59:06 +0530

    calculator next program

--- src/khushi_gautam/method/Calculator_Next.java
+++ src/khushi_gautam/method/Calculator_Next.java
@@ -10,32 +10,31 @@ public class Calculator_Next {
     public int multiply(int a, int b) {return a * b;}
     public int divide(int a, int b) {return a / b;}
 
+
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         System.out.println("Welcome to the calculator app");
-
-        System.out.println("please enter value of A");
         int a = Sc.nextInt();
-        System.out.println("please enter value of B");
-        int b = Sc.nextInt();
-        System.out.println("choose an operator:+, -, *, /");
         char operator = Sc.next().charAt(0);
+        int b = Sc.nextInt();
         Calculator_Next obj = new Calculator_Next();
         switch (operator) {
             case '+':
                 int result = obj.add(a, b);
-                System.out.println(result);
+                System.out.print(result);
                 break;
             case '-':
                 int result1 = obj.sub(a, b);
-                System.out.println(result1);
+                System.out.print(result1);
             case '*':
                 int result2 = obj.multiply(a, b);
-                System.out.println(result2);
+                System.out.print(result2);
                 break;
             case '/':
                 int result3 = obj.divide(a, b);
-                System.out.println(result3);
+                System.out.print(result3);
+                break;
+
             default:
                 System.out.println("Please enter the right operator");
         }
