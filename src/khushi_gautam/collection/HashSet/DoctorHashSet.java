fbb780013de711971cfac1a8d16ccdf4ba89deb8
1994Khushi <ayangelyoung@gmai.com>
2022-11-12 11:39:35 +0530

    DoctorHashSet override

--- src/khushi_gautam/collection/HashSet/DoctorHashSet.java
+++ src/khushi_gautam/collection/HashSet/DoctorHashSet.java
@@ -8,12 +8,26 @@ public class DoctorHashSet {
         HashSet <Doctor> d = new HashSet<>();
         //todo SYNTAX: HashSet <DataType> objName = new HashSet<> ();
         Doctor d1= new Doctor("Dr. Ramesh","JNM",4,"MD");
+        System.out.println("Printing hashcode of doctor1: "+d1.hashCode());
+
         Doctor d2 = new Doctor("Dr. Rohini","JNM",7,"surgeon");
+        System.out.println("Printing hashcode of doctor2: "+d2.hashCode());
+
         Doctor d3 = new Doctor("Dr. Harsha","JNM",9,"Technician");
+        System.out.println("Printing hashcode of doctor3: "+d3.hashCode());
+
         Doctor d4 = new Doctor("Dr. Amit","JNM",7,"neurologist");
+        System.out.println("Printing hashcode of doctor4: "+d4.hashCode());
+
         Doctor d5 = new Doctor("Dr. Rohini","JNM",7,"surgeon");
+        System.out.println("Printing hashcode of doctor5: "+d5.hashCode());
+
         Doctor d6 = new Doctor("Dr. Harsha","JNM",9,"Technician");
+        System.out.println("Printing hashcode of doctor6: "+d6.hashCode());
+
         Doctor d7= new Doctor("Dr. Ramesh","JNM",4,"MD");
+        System.out.println("Printing hashcode of doctor7: "+d7.hashCode());
+
         d.add(d1);
         d.add(d2);
         d.add(d3);
--- src/khushi_gautam/collection/model/Doctor.java
+++ src/khushi_gautam/collection/model/Doctor.java
@@ -1,10 +1,26 @@
 package khushi_gautam.collection.model;
 
+import java.util.Objects;
+
 public class Doctor {
     String Name;
     String Hospital;
     int experience;
     String Post;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return experience == doctor.experience && Name.equals(doctor.Name) &&
+                Hospital.equals(doctor.Hospital) && Post.equals(doctor.Post);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Name, Hospital, experience, Post);
+    }
 
     //todo parameterized constructor
     public Doctor(String name, String hospital, int experience, String post) {
