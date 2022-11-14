5f5029626b56eb8b7e5350af9ecc371226a0153e
Amit <kumaramit21011994@gmail.com>
2022-11-14 17:53:26 +0530

    Manipulation program

---
+++ src/vaiablemanipulationprrogram/manipulaton.java
@@ -0,0 +1,71 @@
+package vaiablemanipulationprrogram;
+
+public class manipulaton {
+
+    public static void main(String args[]){
+
+        int a = 28;
+        int b = 25;
+
+        // subtraction
+        int minus= a-b;
+        System.out.println(minus);
+
+        int l = 9;
+        int m = 6;
+        // multiply
+        int multiply = l*m;
+        System.out.println(l*m);
+
+
+        int p = 97;
+        int q = 4;
+         // devide
+
+        int devide = 90/4;
+
+        System.out.println(p/q);
+
+        int result = a%b;
+        // modulus
+
+        System.out.println(result);
+
+        System.out.println(p%q);
+
+        int w = 99;
+        int c = 200;
+
+        //unary
+        w+=c;
+        // w+ means  w= w+c;
+
+        System.out.println(+w);
+
+        w-=c;
+
+        // -w means w= w-c;
+        System.out.println(-w);
+
+
+        w++;
+        System.out.println(+w);
+
+
+        int r= 13;
+        r--;
+        System.out.println(-r);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
