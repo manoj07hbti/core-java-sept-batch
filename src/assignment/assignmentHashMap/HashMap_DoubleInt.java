6f37eb6fa02e0f756bdef360a964db94a41cbfee
ankitchauhan26 <68079328+ankitchauhan26@users.noreply.github.com>
2022-11-15 19:16:37 +0530

    Assignment_HashCode,EmployeeHashCode

---
+++ src/assignment/assignmentHashMap/HashMap_DoubleInt.java
@@ -0,0 +1,42 @@
+package assignment.assignmentHashMap;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashMap_DoubleInt {
+
+
+    public HashMap <Double,Double> createMapDouble(){
+
+        HashMap <Double,Double> studentMarks = new HashMap<Double, Double>();
+
+
+        studentMarks.put(20.37,33.32);
+        studentMarks.put(22.37,23.32);
+        studentMarks.put(24.37,20.32);
+
+
+        System.out.println("Printing Map : " + studentMarks);
+        return (studentMarks);
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        HashMap_DoubleInt objDouble = new HashMap_DoubleInt();
+        HashMap <Double,Double> studentMarks = objDouble.createMapDouble();
+
+        Set <Double> keys  = studentMarks.keySet() ;
+        for(double var: keys ){
+
+            System.out.println("Printing Student Marks : " + studentMarks.get(keys));
+
+        }
+
+
+
+    }
+}
---
+++ src/assignment/assignmentHashMap/HashMap_Sttring.java
@@ -0,0 +1,46 @@
+package assignment.assignmentHashMap;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashMap_Sttring {
+
+    public HashMap <String,Integer> createMap(){
+    //Syntax : HashMap <datatype_key,datatype_value> varName = new HashMap<>();
+
+        HashMap <String,Integer> studentInfo = new HashMap<>();
+
+        //adding value to Map
+
+        studentInfo.put("Ankit",12);
+        studentInfo.put("Lovkesh",20);
+        studentInfo.put("Kishan",21);
+
+       //  System.out.println("Printing Map : " + studentInfo);
+
+
+        return (studentInfo);
+       // System.out.println("Printing HashMap : "+ studentInfo);
+    }
+
+    public static void main(String[] args) {
+        HashMap_Sttring obj = new HashMap_Sttring();
+        HashMap<String,Integer> studentInfo = obj.createMap();
+
+        //get method : get (key);
+        System.out.println( "Reading Map : " + studentInfo.get("Kishan"));
+        System.out.println( "Reading Map : " + studentInfo.get("Ankit"));
+
+
+        //how to print all element in Map
+
+        Set <String> keys = studentInfo.keySet();
+
+        for (String var : keys){
+
+            System.out.println("Printing Keys In Map : " + studentInfo.get(keys));
+        }
+
+
+    }
+}
