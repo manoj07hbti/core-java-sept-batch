658cd5375d1be73721d18891e2a6fffecdbbf630
1994Khushi <ayangelyoung@gmai.com>
2022-11-18 16:22:45 +0530

    AgeNotEligibleException

---
+++ src/khushi_gautam/Excaption/VotingApplication.java
@@ -0,0 +1,26 @@
+package khushi_gautam.Excaption;
+
+import java.util.Scanner;
+
+public class VotingApplication {
+    public void doVote(int age) throws AgeNotEligibleException{
+        if (age<18){
+            throw new AgeNotEligibleException("Voter is less than 18 so not eligible for vote");
+        } else {
+            System.out.println("welcome to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to voting app");
+        int age = sc.nextInt();
+        VotingApplication obj = new VotingApplication();
+        try {
+            obj.doVote(age);
+        }
+        catch (AgeNotEligibleException e){
+            e.printStackTrace();
+        }
+    }
+}
