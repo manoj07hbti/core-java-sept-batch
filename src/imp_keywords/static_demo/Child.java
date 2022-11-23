a10fd9089133f6ecd2b1bc0eaea979766178cd69
60manojkumar@gmail.com <Motivated@2019>
2022-11-23 19:45:27 +0530

    return type

--- src/imp_keywords/static_demo/Child.java
+++ src/imp_keywords/static_demo/Child.java
@@ -2,9 +2,11 @@ package imp_keywords.static_demo;
 
 public class Child extends DemoStatic {
 
-    public static void m1(){
+    public static String m1(){
 
         System.out.println("This is CHILD STATIC METHOD M1");
+
+        return "ABC";
     }
 
     public static void main(String[] args) {
--- src/imp_keywords/static_demo/DemoStatic.java
+++ src/imp_keywords/static_demo/DemoStatic.java
@@ -1,14 +1,18 @@
 package imp_keywords.static_demo;
 
+
+
 public class DemoStatic {
 
     //TODO a data which does not have static keyword will be called non static or data which belongs to object
 
     String name="Java";
 
-    public static void m1(){
+    public static String m1(){
 
         System.out.println("This is STATIC METHOD M1 ");
+
+        return "HELLO STATIC";
     }
 
 
@@ -16,7 +20,10 @@ public class DemoStatic {
 
     //SYNTAX: ClassName.MethodName();
 
-        DemoStatic.m1();
+      String msg=  DemoStatic.m1();
+
+
+      System.out.println("out put : "+msg);
 
     }
 
